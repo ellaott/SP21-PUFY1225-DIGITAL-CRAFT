ed343a9bbc05071fadd7432e04563e86f7fe7d01
ellaott <otte004@newschool.edu>
2021-04-17 13:56:59 -0500

    sketch3

---
+++ js/p5sketch3.js
@@ -0,0 +1,143 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(63,124,172);
+  
+  
+  stroke(212, 160, 167);
+  strokeWeight(3);
+  
+  var x = 0
+  
+  while (x <= 400) {
+    
+    fill(93, 162, 113);
+    ellipse(x, 0, 50, 50);
+
+    x = x + 50;
+    
+    
+  }
+  stroke(200, 70, 48);
+  strokeWeight(3);
+  var x = 0
+  
+  while (x <= 400) {
+    
+    fill(123,223,242);
+    ellipse(x, 45, 50, 50);
+
+    x = x + 50;
+    
+    
+  }
+  stroke(31,96,94);
+  strokeWeight(3);
+  var x = 0
+  
+  while (x <= 400) {
+    
+    fill(255,227,129);
+    ellipse(x, 90, 50, 50);
+
+    x = x + 50;
+    
+    
+  }
+stroke(212, 160, 167);
+  strokeWeight(3);
+  
+  var x = 0
+  
+  while (x <= 400) {
+    
+    fill(93, 162, 113);
+    ellipse(x, 135, 50, 50);
+
+    x = x + 50;
+    
+    
+  }
+  stroke(200, 70, 48);
+  strokeWeight(3);
+  var x = 0
+  
+  while (x <= 400) {
+    
+    fill(123,223,242);
+    ellipse(x, 180, 50, 50);
+
+    x = x + 50;
+    
+    
+  }
+  stroke(31,96,94);
+  strokeWeight(3);
+  var x = 0
+  
+  while (x <= 400) {
+    
+    fill(255,227,129);
+    ellipse(x, 225, 50, 50);
+
+    x = x + 50;
+    
+    
+  }
+  stroke(212, 160, 167);
+  strokeWeight(3);
+  
+  var x = 0
+  
+  while (x <= 400) {
+    
+    fill(93, 162, 113);
+    ellipse(x, 265, 50, 50);
+
+    x = x + 50;
+    
+    
+  }
+  stroke(200, 70, 48);
+  strokeWeight(3);
+  var x = 0
+  
+  while (x <= 400) {
+    
+    fill(123,223,242);
+    ellipse(x, 310, 50, 50);
+
+    x = x + 50;
+    
+    
+  }
+  stroke(31,96,94);
+  strokeWeight(3);
+  var x = 0
+  
+  while (x <= 400) {
+    
+    fill(255,227,129);
+    ellipse(x, 355, 50, 50);
+
+    x = x + 50;
+    
+    
+  }
+  stroke(212, 160, 167);
+  strokeWeight(3);
+  
+  var x = 0
+  
+  while (x <= 400) {
+    
+    fill(93, 162, 113);
+    ellipse(x, 400, 50, 50);
+
+    x = x + 50;
+    
+    
+  }
+}
